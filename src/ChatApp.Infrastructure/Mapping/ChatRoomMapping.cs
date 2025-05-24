4acb650828191755e781c796912ad6343e39badd
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-24 15:51:03 -0300

    refactor: simplificar configuração do mapeamento de ChatRoom

--- src/ChatApp.Infrastructure/Mapping/ChatRoomMapping.cs
+++ src/ChatApp.Infrastructure/Mapping/ChatRoomMapping.cs
@@ -9,44 +9,39 @@ public class ChatRoomMapping : IEntityTypeConfiguration<ChatRoom>
 {
     public void Configure(EntityTypeBuilder<ChatRoom> builder)
     {
-        builder.ToTable("ChatRooms");
+        {
+            builder.ToTable("ChatRooms");
 
-        builder.HasKey(cr => cr.Id);
+            builder.HasKey(c => c.Id);
 
-        builder.Property(cr => cr.Id)
-            .HasColumnName("Id");
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
 
-        builder.Property(cr => cr.Name)
-            .HasColumnName("Name")
-            .IsRequired()
-            .HasMaxLength(100);
+            builder.Property(c => c.Password)
+                .IsRequired()
+                .HasMaxLength(100);
 
-        builder.Property(cr => cr.OwnerId)
-            .HasColumnName("OwnerId")
-            .IsRequired();
+            builder.Property(c => c.OwnerId)
+                .IsRequired();
 
-        builder.Property(cr => cr.CreatedAt)
-            .HasColumnName("CreatedAt")
-            .IsRequired()
-            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+            builder.Property(c => c.IsPrivate)
+                .IsRequired();
 
-        builder.Property(cr => cr.Password)
-            .HasColumnName("Password")
-            .IsRequired()
-            .HasMaxLength(100);
+            builder.Property(c => c.CreatedAt)
+                .IsRequired();
 
-        builder.Property(cr => cr.IsPrivate)
-            .HasColumnName("IsPrivate")
-            .IsRequired()
-            .HasDefaultValue(false);
+            builder.HasMany(typeof(ChatRoomUser), "_members")
+                .WithOne()
+                .HasForeignKey("ChatRoomId")
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
 
-        builder.Property(cr => cr.Members)
-            .HasColumnName("Members")
-            .IsRequired()
-            .HasDefaultValue(0);
+            // Configura a propriedade _members como campo privado (backing field)
+            builder.Navigation(nameof(ChatRoom.Members))
+                .UsePropertyAccessMode(PropertyAccessMode.Field);
 
-        builder.HasMany(cr => cr.Members)
-            .WithOne()
-            .OnDelete(DeleteBehavior.Cascade);
+        }
     }
+
 }
