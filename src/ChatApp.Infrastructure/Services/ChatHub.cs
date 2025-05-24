84dabb73b58c0cca36b5f51c3271b5aac8a987c3
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-24 15:51:51 -0300

    feat: implementar métodos de conexão e envio de mensagens no ChatHub

--- src/ChatApp.Infrastructure/Services/ChatHub.cs
+++ src/ChatApp.Infrastructure/Services/ChatHub.cs
@@ -1,10 +1,31 @@
 using ChatApp.Application.Abstractions.Services;
+using ChatApp.Domain.Entities.Users;
 
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatApp.Infrastructure.Services;
 
-public class ChatHub : Hub<IChatHub>
+public class ChatHub : Hub<IChatHub>, IChatHub
 {
+    private readonly IHubContext<ChatHub> _hubContext;
 
+    public ChatHub(IHubContext<ChatHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        return base.OnConnectedAsync();
+    }
+
+    public Task JoinGroup(User user, string groupName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendMessageAsync(string user, string message)
+    {
+        return _hubContext.Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
 }
