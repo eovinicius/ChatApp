fb787b008e0e911d11a14d1e9cfdfe78f5e60d51
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-24 15:52:04 -0300

    refactor: ajustar injeção de dependências e configuração de autenticação

--- src/ChatApp.Infrastructure/DependencyInjection.cs
+++ src/ChatApp.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,11 @@
 ﻿using ChatApp.Application.Abstractions.Authentication;
 using ChatApp.Application.Abstractions.Data;
+using ChatApp.Application.Abstractions.Services;
 using ChatApp.Domain.Repositories;
 using ChatApp.Infrastructure.Authentication;
 using ChatApp.Infrastructure.Data;
 using ChatApp.Infrastructure.Repositories;
+using ChatApp.Infrastructure.Services;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -18,14 +20,16 @@ public static class DependencyInjection
         IConfiguration configuration)
     {
         AddPersistence(services, configuration);
-        AddAuthentication(services, configuration);
+        AddAuthentication(services);
+        AddServicesProviders(services);
         return services;
     }
 
     private static void AddPersistence(IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("Database") ??
-                               throw new ArgumentNullException(nameof(configuration));
+        // var connectionString = configuration.GetConnectionString("Database") ??
+        //                        throw new ArgumentNullException(nameof(configuration));
+        var connectionString = "Host=localhost;Port=5432;Database=chatapp;Username=postgres;Password=postgres;";
 
         services.AddDbContext<ChatAppDbContext>(options => options.UseNpgsql(connectionString));
 
@@ -35,9 +39,17 @@ public static class DependencyInjection
         services.AddScoped<IUnitOfWork, UnitOfWork>();
     }
 
-    private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
+    private static void AddServicesProviders(IServiceCollection services)
+    {
+        services.AddScoped<IChatHub, ChatHub>();
+        services.AddSignalR();
+    }
+
+    private static void AddAuthentication(IServiceCollection services)
     {
         services.AddHttpContextAccessor();
         services.AddScoped<IUserContext, UserContext>();
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<IHashService, HashService>();
     }
 }
