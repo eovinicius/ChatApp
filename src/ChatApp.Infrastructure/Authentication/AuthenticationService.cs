c54127251944b368ae0a87ebefabdfb515057cf7
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-24 15:51:41 -0300

    feat: implementar serviço de autenticação com geração de token JWT

---
+++ src/ChatApp.Infrastructure/Authentication/AuthenticationService.cs
@@ -0,0 +1,43 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using ChatApp.Application.Abstractions.Authentication;
+using ChatApp.Domain.Entities.Users;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ChatApp.Infrastructure.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthenticationService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string? GenerateToken(User user)
+    {
+        var handler = new JwtSecurityTokenHandler();
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(
+            [
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            ]),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes("382130921319238012309")),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = handler.CreateToken(tokenDescriptor);
+        var tokenString = handler.WriteToken(token);
+        return tokenString;
+    }
+}
