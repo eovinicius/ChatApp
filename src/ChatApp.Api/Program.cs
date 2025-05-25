3966aab347665a34f415e898c9cc9c515b4c1ab1
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-24 22:17:12 -0300

    feat: adicionar configuração do Swagger com suporte a autenticação JWT

--- src/ChatApp.Api/Program.cs
+++ src/ChatApp.Api/Program.cs
@@ -1,6 +1,8 @@
 using ChatApp.Application;
 using ChatApp.Infrastructure;
 
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
@@ -11,6 +13,35 @@ builder.Services.AddControllers();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ChatApp API", Version = "v1" });
+
+    c.AddSecurityDefinition("Token", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Description = "Insira o token JWT aqui",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Token"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
