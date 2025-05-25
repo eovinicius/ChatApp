9fccfdd1c9415c4744418cb82e8c66d87c666b07
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-24 22:19:41 -0300

    refactor: ajustar mapeamento da entidade ChatRoom para propriedades de comprimento adequado

--- src/ChatApp.Infrastructure/Mapping/ChatRoomMapping.cs
+++ src/ChatApp.Infrastructure/Mapping/ChatRoomMapping.cs
@@ -9,39 +9,29 @@ public class ChatRoomMapping : IEntityTypeConfiguration<ChatRoom>
 {
     public void Configure(EntityTypeBuilder<ChatRoom> builder)
     {
-        {
-            builder.ToTable("ChatRooms");
+        builder.ToTable("ChatRooms");
 
-            builder.HasKey(c => c.Id);
+        builder.HasKey(c => c.Id);
 
-            builder.Property(c => c.Name)
-                .IsRequired()
-                .HasMaxLength(100);
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(50);
 
-            builder.Property(c => c.Password)
-                .IsRequired()
-                .HasMaxLength(100);
+        builder.Property(c => c.Password)
+            .IsRequired()
+            .HasMaxLength(30);
 
-            builder.Property(c => c.OwnerId)
-                .IsRequired();
+        builder.Property(c => c.OwnerId)
+            .IsRequired();
 
-            builder.Property(c => c.IsPrivate)
-                .IsRequired();
+        builder.Property(c => c.IsPrivate)
+            .IsRequired();
 
-            builder.Property(c => c.CreatedAt)
-                .IsRequired();
+        builder.Property(c => c.CreatedAt)
+            .IsRequired();
 
-            builder.HasMany(typeof(ChatRoomUser), "_members")
-                .WithOne()
-                .HasForeignKey("ChatRoomId")
-                .IsRequired()
-                .OnDelete(DeleteBehavior.Cascade);
-
-            // Configura a propriedade _members como campo privado (backing field)
-            builder.Navigation(nameof(ChatRoom.Members))
-                .UsePropertyAccessMode(PropertyAccessMode.Field);
-
-        }
+        builder.Metadata
+            .FindNavigation(nameof(ChatRoom.Members))!
+            .SetPropertyAccessMode(PropertyAccessMode.Field);
     }
-
 }
