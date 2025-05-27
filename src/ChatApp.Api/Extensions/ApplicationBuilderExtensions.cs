2fc7f4d63ee197e7fa4af6e4bf5d4f92c9329554
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-27 14:51:33 -0300

    feat: adicionar extensões para gerenciamento de migrações e manipulação de exceções

---
+++ src/ChatApp.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using ChatApp.Api.Middlewares;
+using ChatApp.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        using var dbContext = scope.ServiceProvider.GetRequiredService<ChatAppDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+
+    public static void UseRequestContextLogging(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<RequestContextLoggingMiddleware>();
+    }
+}
