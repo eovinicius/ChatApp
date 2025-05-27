699ef1b2c07f8c20f667e5dc40d5df2c814e6368
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-27 14:52:03 -0300

    feat: adicionar middleware para tratamento de exceções

---
+++ src/ChatApp.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Api.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Ocorreu uma exceção: {Message}", exception.Message);
+
+            var exceptionDetails = GetExceptionDetails(exception);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = exceptionDetails.Status,
+                Type = exceptionDetails.Type,
+                Title = exceptionDetails.Title,
+                Detail = exceptionDetails.Detail,
+            };
+
+            context.Response.StatusCode = exceptionDetails.Status;
+            context.Response.ContentType = "application/problem+json";
+
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+
+    private static ExceptionDetails GetExceptionDetails(Exception exception)
+    {
+        return exception switch
+        {
+            _ => new ExceptionDetails(
+            StatusCodes.Status500InternalServerError,
+            "Server Error",
+            "Erro interno do servidor",
+            "Ocorreu um erro inesperado",
+            null)
+        };
+    }
+
+    internal record ExceptionDetails(
+        int Status,
+        string Type,
+        string Title,
+        string Detail,
+        IEnumerable<object>? Errors);
+}
