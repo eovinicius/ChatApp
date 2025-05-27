1241d1f4c62bd29dffb7ff2aa05aa60add62f414
Vinicius Oliveira <eovinicius10@gmail.com>
2025-05-27 16:28:03 -0300

    feat: adicionar suporte a Correlation ID e configurar logging com Serilog

--- src/ChatApp.Api/Program.cs
+++ src/ChatApp.Api/Program.cs
@@ -4,6 +4,9 @@ using ChatApp.Api.Extensions;
 using ChatApp.Application;
 using ChatApp.Infrastructure;
 
+using CorrelationId;
+using CorrelationId.DependencyInjection;
+
 using Microsoft.OpenApi.Models;
 
 using Serilog;
@@ -11,8 +14,30 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.Host.UseSerilog((context, loggerConfig) =>
-//     loggerConfig.ReadFrom.Configuration(context.Configuration));
+builder.Services.AddDefaultCorrelationId(options =>
+{
+    options.AddToLoggingScope = true;
+    options.RequestHeader = "X-Correlation-ID";
+    options.ResponseHeader = "X-Correlation-ID";
+    options.IncludeInResponse = true;
+});
+
+builder.Host.UseSerilog((context, loggerConfig) =>
+    loggerConfig
+        // .ReadFrom.Configuration(context.Configuration)
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+        .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Warning)
+        .Enrich.WithCorrelationId()
+        .Enrich.FromLogContext()
+        .WriteTo.Console(outputTemplate:
+            "[{Timestamp:HH:mm:ss} {Level:u3}] (CorrID: {CorrelationId}) {Message:lj}{NewLine}{Exception}")
+        .WriteTo.File(
+            path: "logs/log-.txt",
+            rollingInterval: RollingInterval.Day,
+            outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] (CorrID: {CorrelationId}) {Message:lj}{NewLine}{Exception}")
+);
+
 
 builder.Services.AddCors();
 
@@ -81,11 +106,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCorrelationId();
+
 app.UseRateLimiter();
 
 app.UseCustomExceptionHandler();
 
-// app.UseSerilogRequestLogging();
+app.UseRequestContextLogging();
 
 app.UseCors();
 
