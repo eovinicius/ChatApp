db789bf728c702e7a0ac9e292eba40d4567fccac
Vinicius Oliveira <eovinicius10@gmail.com>
2025-06-28 21:45:13 -0300

    feat: adiciona propriedade IsFailure à classe Result

--- src/ChatApp.Domain/Abstractions/Result.cs
+++ src/ChatApp.Domain/Abstractions/Result.cs
@@ -5,6 +5,7 @@ namespace ChatApp.Domain.Abstractions;
 public class Result
 {
     public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
     public Error Error { get; }
 
     public Result(bool isSuccess, Error error)
