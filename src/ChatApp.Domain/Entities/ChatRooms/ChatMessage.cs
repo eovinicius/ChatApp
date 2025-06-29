361b19263dbac115080fbe0fa81f953bd8a90a05
Vinicius Oliveira <eovinicius10@gmail.com>
2025-06-28 21:47:37 -0300

    feat: inclui métodos de validação para atualização e remoção de mensagens

--- src/ChatApp.Domain/Entities/ChatRooms/ChatMessage.cs
+++ src/ChatApp.Domain/Entities/ChatRooms/ChatMessage.cs
@@ -1,19 +1,22 @@
+using ChatApp.Domain.Abstractions;
+
 namespace ChatApp.Domain.Entities.ChatRooms;
 
 public sealed class ChatMessage
 {
     public Guid Id { get; private set; }
-    public Guid ChatRoom { get; private set; }
+    public Guid ChatRoomId { get; private set; }
     public Guid SenderId { get; private set; }
     public string Content { get; private set; }
     public DateTime SentAt { get; private set; }
+    public bool IsEdited { get; private set; }
 
     private ChatMessage() { }
 
     public ChatMessage(Guid chatRoomId, Guid userId, string content)
     {
         Id = Guid.NewGuid();
-        ChatRoom = chatRoomId;
+        ChatRoomId = chatRoomId;
         SenderId = userId;
         Content = content;
         SentAt = DateTime.UtcNow;
@@ -22,5 +25,45 @@ public sealed class ChatMessage
     public static ChatMessage Create(Guid chatRoomId, Guid userId, string content)
     {
         return new ChatMessage(chatRoomId, userId, content);
+    }
+
+    public bool CanBeDeletedBy(Guid userId, DateTime utcNow, int timeLimitInHours)
+    {
+        if (SenderId != userId)
+            return false;
+
+        if (SentAt < utcNow.AddHours(-timeLimitInHours))
+            return false;
+
+        return true;
+    }
+
+    public Result Update(string newContent, Guid userId, DateTime utcNow, int timeLimitInHours = 6)
+    {
+        if (!CanBeEditBy(userId, utcNow, timeLimitInHours))
+        {
+            return Result.Failure(Error.None);
+        }
+
+        if (string.IsNullOrWhiteSpace(newContent))
+        {
+            return Result.Failure(Error.None);
+        }
+
+        Content = newContent;
+        IsEdited = true;
+
+        return Result.Success();
+    }
+
+    public bool CanBeEditBy(Guid userId, DateTime utcNow, int timeLimitInHours = 6)
+    {
+        if (SenderId != userId)
+            return false;
+
+        if (SentAt < utcNow.AddHours(-timeLimitInHours))
+            return false;
+
+        return true;
     }
 }
