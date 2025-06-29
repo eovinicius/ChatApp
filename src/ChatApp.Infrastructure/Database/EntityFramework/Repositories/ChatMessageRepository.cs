eba6c576b6ff91b7c5cb35690ccbfed63c012a39
Vinicius Oliveira <eovinicius10@gmail.com>
2025-06-29 01:46:40 -0300

    feat: adiciona métodos de exclusão, obtenção e atualização de mensagens no repositório

--- src/ChatApp.Infrastructure/Database/EntityFramework/Repositories/ChatMessageRepository.cs
+++ src/ChatApp.Infrastructure/Database/EntityFramework/Repositories/ChatMessageRepository.cs
@@ -1,5 +1,8 @@
+
 using ChatApp.Domain.Entities.ChatRooms;
 using ChatApp.Domain.Repositories;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace ChatApp.Infrastructure.Database.EntityFramework.Repositories;
 
@@ -15,5 +18,21 @@ public class ChatMessageRepository : IChatMessageRepository
     public async Task Add(ChatMessage chatMessage, CancellationToken cancellationToken)
     {
         await _dbContext.Messages.AddAsync(chatMessage, cancellationToken);
+    }
+
+    public void Delete(ChatMessage chatMessage, CancellationToken cancellationToken)
+    {
+        _dbContext.Messages.Remove(chatMessage);
+    }
+
+    public async Task<ChatMessage?> GetById(Guid messageId, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Messages.FirstOrDefaultAsync(x => x.Id == messageId, cancellationToken);
+    }
+
+    public async Task Update(ChatMessage chatMessage, CancellationToken cancellationToken)
+    {
+        _dbContext.Messages.Update(chatMessage);
+        await _dbContext.SaveChangesAsync(cancellationToken);
     }
 }
