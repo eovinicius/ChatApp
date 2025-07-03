0c17291371b5857d12685985d654fdc4380044c2
Vinicius Oliveira <eovinicius10@gmail.com>
2025-07-03 16:12:34 -0300

    refactor: remove factory class e ajusta estrutura da classe ChatMessage

--- src/ChatApp.Domain/Entities/Messages/ChatMessage.cs
+++ src/ChatApp.Domain/Entities/Messages/ChatMessage.cs
@@ -2,14 +2,14 @@ using ChatApp.Domain.Abstractions;
 
 namespace ChatApp.Domain.Entities.Messages;
 
-public sealed class ChatMessage
+public class ChatMessage
 {
     public Guid Id { get; private set; }
     public Guid ChatRoomId { get; private set; }
     public Guid SenderId { get; private set; }
-    public MessageContent Content { get; private set; }
     public DateTime SentAt { get; private set; }
-    public bool IsEdited { get; private set; }
+    public DateTime EditedAt { get; private set; }
+    public ContentType ContentType { get; private set; }
 
     private const int MessageEditTimeLimitInHours = 6;
     private const int MessageDeleteTimeLimitInHours = 24;
@@ -46,16 +46,16 @@ public sealed class ChatMessage
         if (!IsWithinTimeLimit(utcNow, MessageEditTimeLimitInHours))
             return Result.Failure(Error.NullValue);
 
-        if (!IsTextMessage)
-        {
-            return Result.Failure(Error.NullValue);
-        }
+        // if (!IsTextMessage)
+        // {
+        //     return Result.Failure(Error.NullValue);
+        // }
 
         if (string.IsNullOrWhiteSpace(newContent))
             return Result.Failure(Error.NullValue);
 
-        Content = new TextContent(newContent);
-        IsEdited = true;
+        // Content = new TextContent(newContent);
+        EditedAt = utcNow;
 
         return Result.Success();
     }
@@ -63,17 +63,5 @@ public sealed class ChatMessage
     private bool IsWithinTimeLimit(DateTime utcNow, int limitInHours)
     {
         return SentAt >= utcNow.AddHours(-limitInHours);
-    }
-
-    private bool IsTextMessage => Content.Type == MessageContentType.Text;
-
-    public void SetFilePath(string filePath)
-    {
-        Content = Content switch
-        {
-            ImageContent => new ImageContent(filePath),
-            AudioContent => new AudioContent(filePath),
-            _ => throw new InvalidOperationException("só pode ser usado em mensagens de imagem ou áudio."),
-        };
     }
 }
