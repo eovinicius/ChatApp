3371d5bb76f00f8919db6d0bb7a59241d9e5befe
Vinicius Oliveira <eovinicius10@gmail.com>
2025-08-10 20:25:13 -0300

    fix: adiciona token de cancelamento nas chamadas de envio de mensagens

--- src/ChatApp.Infrastructure/Services/SignalRChatRoomNotifier.cs
+++ src/ChatApp.Infrastructure/Services/SignalRChatRoomNotifier.cs
@@ -17,17 +17,17 @@ public class SignalRChatRoomNotifier : IChatHub
     public async Task JoinGroup(string roomId, string user, CancellationToken cancellationToken)
     {
         await _hubContext.Clients.Group(roomId)
-            .SendAsync("UserJoined", user);
+            .SendAsync("UserJoined", user, cancellationToken: cancellationToken);
     }
 
     public async Task LeftGroup(string roomId, string user, CancellationToken cancellationToken)
     {
         await _hubContext.Clients.Group(roomId)
-            .SendAsync("UserLeft", user);
+            .SendAsync("UserLeft", user, cancellationToken: cancellationToken);
     }
 
     public async Task SendMessageToGroup(string roomId, string message, CancellationToken cancellationToken)
     {
-        await _hubContext.Clients.Group(roomId).SendAsync("ReceiveMessage", roomId, message);
+        await _hubContext.Clients.Group(roomId).SendAsync("ReceiveMessage", roomId, message, cancellationToken: cancellationToken);
     }
 }
