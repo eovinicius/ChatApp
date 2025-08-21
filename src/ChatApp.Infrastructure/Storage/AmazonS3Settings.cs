4d9422e3a26f658c2a44d0bd766d0dad4db1c9c8
Vinicius Oliveira <eovinicius10@gmail.com>
2025-08-21 20:49:38 -0300

    refactor: adiciona propriedades Region, AccessKey e SecretKey

--- src/ChatApp.Infrastructure/Storage/AmazonS3Settings.cs
+++ src/ChatApp.Infrastructure/Storage/AmazonS3Settings.cs
@@ -3,4 +3,7 @@
 internal class AmazonS3Settings
 {
     public string BucketName { get; set; } = string.Empty;
+    public string Region { get; set; } = string.Empty;
+    public string? AccessKey { get; set; }
+    public string? SecretKey { get; set; }
 }
