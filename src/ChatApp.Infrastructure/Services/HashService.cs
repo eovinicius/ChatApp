7a2c51c6b30af1a5323de0eee025c1cc544b7044
Vinicius Oliveira <eovinicius10@gmail.com>
2025-08-21 20:37:52 -0300

    rafactor: ajusta o tamanho do salt

--- src/ChatApp.Infrastructure/Services/HashService.cs
+++ src/ChatApp.Infrastructure/Services/HashService.cs
@@ -4,9 +4,10 @@ namespace ChatApp.Infrastructure.Services;
 
 public class HashService : IHashService
 {
+    private const int SaltSize = 16;
     public string Hash(string password)
     {
-        var salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+        var salt = BCrypt.Net.BCrypt.GenerateSalt(SaltSize);
         var passwordHash = BCrypt.Net.BCrypt.HashPassword(password, salt);
         return passwordHash;
     }
@@ -14,5 +15,4 @@ public class HashService : IHashService
     {
         return BCrypt.Net.BCrypt.Verify(password, passwordHash);
     }
-
 }
