0870d9030e826c1ee4cda969eff9c9f1fa9de2e7
Vinicius Oliveira <eovinicius10@gmail.com>
2025-09-27 18:27:11 -0300

    feat: adiciona suporte ao acesso ao contexto de correlação no middleware de registro de contexto de requisição

--- src/ChatApp.Api/Middlewares/RequestContextLoggingMiddleware.cs
+++ src/ChatApp.Api/Middlewares/RequestContextLoggingMiddleware.cs
@@ -1,3 +1,5 @@
+using CorrelationId.Abstractions;
+
 using Serilog.Context;
 
 namespace ChatApp.Api.Middlewares;
@@ -6,17 +8,21 @@ public class RequestContextLoggingMiddleware
 {
     private const string HeaderKey = "X-Correlation-ID";
     private readonly RequestDelegate _next;
+    private readonly ICorrelationContextAccessor _correlationAccessor;
 
-    public RequestContextLoggingMiddleware(RequestDelegate next)
+    public RequestContextLoggingMiddleware(RequestDelegate next, ICorrelationContextAccessor correlationAccessor)
     {
         _next = next;
+        _correlationAccessor = correlationAccessor;
     }
 
     public async Task Invoke(HttpContext context)
     {
-        var correlationId = context.Request.Headers.TryGetValue(HeaderKey, out var cid)
-            ? cid.ToString()
-            : Guid.NewGuid().ToString();
+        // Preferir o ID resolvido pelo middleware do pacote CorrelationId
+        var correlationId = _correlationAccessor.CorrelationContext?.CorrelationId
+            ?? (context.Request.Headers.TryGetValue(HeaderKey, out var cid)
+                ? cid.ToString()
+                : context.TraceIdentifier);
 
         context.Items[HeaderKey] = correlationId;
 
