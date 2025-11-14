8f2af28ccd2a66ed183e8c7994fc31e17b701ac0
Vinicius Oliveira <eovinicius10@gmail.com>
2025-11-14 00:11:48 -0300

    feat: adiciona rate limiter no endpoint de criacao de sala

--- src/ChatApp.Api/Controllers/ChatRoomController.cs
+++ src/ChatApp.Api/Controllers/ChatRoomController.cs
@@ -1,13 +1,16 @@
 using ChatApp.Application.UseCases.Rooms.CreateRoom;
+using ChatApp.Domain.Abstractions;
 
 using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace ChatApp.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[EnableRateLimiting("default")]
 public class ChatRoomController : ControllerBase
 {
     private readonly ISender _sender;
@@ -20,10 +23,16 @@ public class ChatRoomController : ControllerBase
     public async Task<IActionResult> CreateChatRoom([FromBody] CreateChatRoomRequest request)
     {
         var result = await _sender.Send(new CreateChatroomCommand(request.RoomName, request.IsPrivate, request.Password));
+        
+        if (result.IsFailure)
+        {
+            return BadRequest(new { error = result.Error.Code, message = result.Error.Name });
+        }
+
         return CreatedAtAction(
             nameof(CreateChatRoom),
-            new { token = result.Value },
-            result);
+            new { id = result.Value },
+            new { id = result.Value });
     }
 
     public sealed class CreateChatRoomRequest
