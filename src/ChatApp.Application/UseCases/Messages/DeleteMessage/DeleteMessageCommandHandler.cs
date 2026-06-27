542b55fefe9cbfb9e8242e47d9810fc528f409aa
Vinicius Oliveira <eovinicius10@gmail.com>
2026-06-27 12:18:51 -0300

    fix: garante que as alterações sejam salvas após a exclusão da mensagem

--- src/ChatApp.Application/UseCases/Messages/DeleteMessage/DeleteMessageCommandHandler.cs
+++ src/ChatApp.Application/UseCases/Messages/DeleteMessage/DeleteMessageCommandHandler.cs
@@ -53,12 +53,13 @@ public class DeleteMessageCommandHandler : ICommandHandler<DeleteMessageCommand>
 
         _messageRepository.Delete(message, cancellationToken);
 
-        await _unitOfWork.Commit(cancellationToken);
 
         if (message.ContentType != ContentType.Text)
         {
             await _fileStorageService.Delete(message.Content, cancellationToken);
         }
+    
+        await _unitOfWork.Commit(cancellationToken);
 
         return Result.Success();
     }
