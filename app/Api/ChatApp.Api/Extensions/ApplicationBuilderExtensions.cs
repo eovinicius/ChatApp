92a59446ec541afaec48c760c38e2669ac00b476
Vinicius <eovinicius10@gmail.com>
2026-08-13 00:27:09 -0300

    feat: aplica migrations dos dois modulos no startup

--- app/Api/ChatApp.Api/Extensions/ApplicationBuilderExtensions.cs
+++ app/Api/ChatApp.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -2,20 +2,24 @@ using Chat.Infrastructure.Database.EntityFramework;
 
 using ChatApp.Api.Middlewares;
 
+using Identity.Infrastructure.Database;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace ChatApp.Api.Extensions;
 
 public static class ApplicationBuilderExtensions
 {
+    // Cada módulo tem o seu DbContext (schemas "chat" e "identity"), então o host
+    // precisa migrar os dois.
     public static void ApplyMigrations(this IApplicationBuilder app)
     {
         using var scope = app.ApplicationServices.CreateScope();
 
-        using var dbContext = scope.ServiceProvider.GetRequiredService<ChatAppDbContext>();
-
-        dbContext.Database.Migrate();
+        scope.ServiceProvider.GetRequiredService<IdentityDbContext>().Database.Migrate();
+        scope.ServiceProvider.GetRequiredService<ChatAppDbContext>().Database.Migrate();
     }
+
     public static void UseCustomExceptionHandler(this IApplicationBuilder app)
     {
         app.UseMiddleware<ExceptionHandlingMiddleware>();
